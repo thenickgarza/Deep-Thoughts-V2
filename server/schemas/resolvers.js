4fe514295610a199cf0a4f9eb2084276abdd388c
Nick Garza <thenickgarza14@gmail.com>
2023-04-27 14:23:15 -0500

    added authentication error messages for login mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,5 @@
 const { User, Thought } = require("../models");
+const { AuthenticationError } = require('apollo-server-express')
 
 const resolvers = {
   Query: {
@@ -30,8 +31,20 @@ const resolvers = {
 
         return user;
     },
-    login: async () => {
+    login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
 
+        if (!user) {
+            throw new AuthenticationError("Incorrect credentials");
+        }
+
+        const correctPw = await user.isCorrectPassword(password);
+
+        if (!correctPw) {
+            throw new AuthenticationError("Incorrect credentials");
+        }
+
+        return user
     }
   }
 };
