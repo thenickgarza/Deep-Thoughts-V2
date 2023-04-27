3f9ebf08b65f012fea9a25daafbdba7fe8d84b08
Nick Garza <thenickgarza14@gmail.com>
2023-04-27 15:18:52 -0500

    user is succesfully sent JWT token upon login

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,6 @@
 const { User, Thought } = require("../models");
-const { AuthenticationError } = require('apollo-server-express')
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
@@ -28,8 +29,9 @@ const resolvers = {
   Mutation: {
     addUser: async(parent, args) => {
         const user = await User.create(args)
+        const token = signToken(user)
 
-        return user;
+        return { token, user };
     },
     login: async (parent, { email, password }) => {
         const user = await User.findOne({ email });
@@ -44,7 +46,8 @@ const resolvers = {
             throw new AuthenticationError("Incorrect credentials");
         }
 
-        return user
+        const token = signToken(user)
+        return { token, user };
     }
   }
 };
