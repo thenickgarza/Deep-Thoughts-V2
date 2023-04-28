8318723e12f1227493ba28c38ee64c037ea6a853
Nick Garza <thenickgarza14@gmail.com>
2023-04-28 16:53:09 -0500

    added friends list component to the profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -5,6 +5,7 @@ import ThoughtList from '../components/ThoughtList';
 
 import { useQuery } from '@apollo/client';
 import { QUERY_USER } from '../utils/queries';
+import FriendList from '../components/FriendList';
 
 
 const Profile = () => {
@@ -31,6 +32,14 @@ const Profile = () => {
       <div className="flex-row justify-space-between mb-3">
         <div className="col-12 mb-3 col-lg-8">
           <ThoughtList thoughts={user.thoughts} title={`${user.username}'s thoughts...`} />
+        </div>
+
+        <div className='col-12 col-lg-3 mb-3'>
+          <FriendList
+          username={user.username}
+          friendCount={user.friendCount}
+          friends={user.friends}
+          />
         </div>
       </div>
     </div>
